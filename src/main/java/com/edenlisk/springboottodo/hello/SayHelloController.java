6858201ddfad95211f17ee9fcffe62b74078c66f
edenlisk <nkumbuyenock@gmail.com>
2024-04-01 20:00:34 +0200

    Add sayHello controller

---
+++ src/main/java/com/edenlisk/springboottodo/hello/SayHelloController.java
@@ -0,0 +1,33 @@
+package com.edenlisk.springboottodo.hello;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+
+@Controller
+public class SayHelloController {
+
+    @RequestMapping("say-hello")
+    @ResponseBody
+    public String sayHello() {
+        return "Hello. What are you learning today!";
+    }
+
+
+    @RequestMapping("say-hello-html")
+    @ResponseBody
+    public String sayHelloHtml() {
+        StringBuffer str = new StringBuffer();
+        str.append("<html>");
+        str.append("<head>");
+        str.append("<title>");
+        str.append("My first web page");
+        str.append("</title>");
+        str.append("</head>");
+        str.append("<body>Hello World Html from spring boot 3 <strong>controller</strong></body>");
+        str.append("</html>");
+        return str.toString();
+    }
+
+}
